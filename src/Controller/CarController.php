f0a4ec2268da7d49c20c18e6d968cf157c019df1
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-02-07 08:41:22 -0800

    Add carDesc action

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Car;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarController extends Controller
@@ -15,23 +14,50 @@ class CarController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-
+        
         $cars = $em->getRepository(Car::class)->findAll();
-
+        
         return $this->render('car/index.html.twig', array(
             'cars' => $cars,
         ));
     }
-
-  /**
-   * Finds and displays a car entity.
-   *
-   * @Route("/car/{id}", name="car_show")
-   */
-  public function showAction(Car $car)
-  {
-    return $this->render('car/show.html.twig', array(
-      'car' => $car,
-    ));
-  }
+    
+    /**
+     * Finds and displays a car entity.
+     *
+     * @Route("/car/{id}", name="car_show")
+     */
+    public function showAction(Car $car)
+    {
+        return $this->render('car/show.html.twig', array(
+            'car' => $car,
+        ));
+    }
+    
+    /**
+     * @Route("/car_desc", name="car_desc")
+     */
+    public function carDesc()
+    {
+        // Call Entity Manager
+        $em = $this->getDoctrine()->getManager();
+        
+        // Create Query
+        $query = $em->createQuery(
+            "
+                SELECT c
+                FROM App\Entity\Car c
+                ORDER BY c.travelledDistance DESC
+                "
+        );
+        
+        // Execute Query
+        $result = $query->getResult();
+        
+        // Send result to View for rendering
+        return $this->render("car/index.html.twig", [
+            "cars" => $result,
+        ]);
+    }
+    
 }
